c227c599e7dea070e50df7a54213618197d29e47
Ivonne Ruiz <Ivonne.Ruiz@ellucian.com>
2019-07-25 15:58:09 -0500

    Implement button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+﻿import React from 'react';
+
+// MUI
+import MuiButton from '@material-ui/core/Button';
+
+export type ButtonColor = 'default' | 'primary' | 'secondary';
+
+export interface IButtonProps {
+    children: React.ReactNode;
+    classes?: object;
+    className?: string;
+    color?: ButtonColor;
+    disabled?: boolean;
+    loading?: boolean;
+    noFluid?: boolean;
+    fluid?: boolean;
+    href?: string;
+    id: string;
+    size?: 'small' | 'medium' | 'large';
+    style?: object;
+    type?: string;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button: React.FC<IButtonProps> = (props: IButtonProps): JSX.Element => {
+    const {
+        children,
+        ...rest
+    } = props;
+
+    return (
+        <MuiButton
+            {...rest}
+        >
+            {children}
+        </MuiButton>
+    );
+};
+
+export default Button;
